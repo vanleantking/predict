5e5f540c462188b945825d9bcd07e855d60962d2
levan <vanle@tctav.com>
2018-04-26 13:54:09 +0700

    read files

--- read.py
+++ read.py
@@ -1,4 +1,16 @@
 import arff
+import numpy as np
+import os
 
-data = arff.load(open('1year.arff'))
-print(data['data'][:10])
+BASE_DATA = 'datasets\data'
+def loaddata():
+    for file in os.listdir(BASE_DATA):
+        filepath = os.path.join(BASE_DATA, file)
+        data = arff.load(open(filepath))
+        print(data['data'][:10])
+        return
+
+if __name__ == "__main__":
+    loaddata()
+# data = arff.load(open('1year.arff'))
+# print(data['data'][:10])
